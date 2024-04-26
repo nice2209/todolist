f09049f9731421b3aefcf70783414cbfddc0ca0d
nice2209 <164824356+nice2209@users.noreply.github.com>
2024-04-26 17:13:14 +0900

    Add files via upload

---
+++ todolist/script.js
@@ -0,0 +1,20 @@
+const todoList = document.getElementById('todoList');
+
+function add() {
+    const todoInput = document.getElementById('todoInput');
+    if (todoInput.value.trim() !== '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '삭제';
+        deleteButton.classList.add('delete-btn');
+        deleteButton.addEventListener('click', function () {
+            li.remove();
+        });
+        li.textContent = todoInput.value.trim();
+        li.appendChild(deleteButton);
+        todoList.appendChild(li);
+        todoInput.value = '';
+    } else {
+        alert('할일을 적어주세요.');
+    }
+}
