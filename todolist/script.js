aaa8f2545a1c330356ad31b28f437975b26bcb03
nice2209 <164824356+nice2209@users.noreply.github.com>
2024-04-26 18:59:58 +0900

    script.js

--- todolist/script.js
+++ todolist/script.js
@@ -4,14 +4,20 @@ function add() {
     const todoInput = document.getElementById('todoInput');
     if (todoInput.value.trim() !== '') {
         const li = document.createElement('li');
+
         const deleteButton = document.createElement('button');
+
         deleteButton.textContent = '삭제';
         deleteButton.classList.add('delete-btn');
+
         deleteButton.addEventListener('click', function () {
             li.remove();
         });
+
         li.textContent = todoInput.value.trim();
         li.appendChild(deleteButton);
+        li.style.wordWrap = "break-word";
+        
         todoList.appendChild(li);
         todoInput.value = '';
     } else {
